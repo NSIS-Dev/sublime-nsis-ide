4764d52a75d8f41f59668be3580bc0269d26c2d7
Jan T. Sott <jan@idleberg.com>
2016-05-13 18:35:30 +0200

    update from Gist

--- chmod.py
+++ chmod.py
@@ -1,13 +1,26 @@
+# https://gist.github.com/idleberg/03bc3766c760bb4b81e3
+
 import os, stat, sublime, sublime_plugin
 
-# Configuration
-pkg = 'NSIS Developer'
-script = sublime.packages_path() + '/' + pkg + '/Support/makensis.sh'
+# Package name, must match directory name
+package = 'makensis'
+
+# Array of files, relative to package directory
+files = [
+    'Support/makensis.sh'
+]
 
 def plugin_loaded():
+    from os.path import join
     from package_control import events
 
-    # chmod +x <script>
-    if (events.install(pkg) or events.post_upgrade(pkg)) and os.name is 'posix' or 'mac':
-        st = os.stat(script)
-        os.chmod(script, st.st_mode | stat.S_IEXEC)
+    if (events.install(package) or events.post_upgrade(package)) and os.name is 'posix' or 'mac':
+        for file in files:
+
+            # Concat full path
+            f = join(sublime.packages_path(), package + '/' + file)
+
+            # Change permissions, if file exists
+            if os.path.isfile(f):
+                st = os.stat(f)
+                os.chmod(f, st.st_mode | stat.S_IEXEC)
